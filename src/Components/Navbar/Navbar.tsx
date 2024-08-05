7086bc25b75275e1026d21f6e60fec5630d7641d
hakim-asrori <saneglos005@gmail.com>
2024-08-05 13:09:55 +0700

    update baru navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           <li><Link spy={true} to="services" smooth={true} >Services</Link></li>
           <li><Link spy={true} to="experience" smooth={true} >Experience</Link></li>
           <li><Link spy={true} to="portfolio" smooth={true} >Portfolio</Link></li>
-          <li><Link spy={true} to="testimonial" smooth={true} >Testimonials</Link></li>
+          {/* <li><Link spy={true} to="testimonial" smooth={true} >Testimonials</Link></li> */}
         </ul>
       </div>
       <button className="button n-button">
